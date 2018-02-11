c5ce8b9c461052bdedd997ad0158ad4ba8bbdccb
MAIN\cxs5805 <cxs5805@g.rit.edu>
2018-02-10 22:16:42 -0500

    Created camera script + UI, textured mine brown

---
+++ Team7_Project1/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // member data for each camera
+    public GameObject IsometricCamera;
+    public GameObject ThirdPersonCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            IsometricCamera.SetActive(!IsometricCamera.activeSelf);
+            ThirdPersonCamera.SetActive(!ThirdPersonCamera.activeSelf);
+        }
+	}
+}
