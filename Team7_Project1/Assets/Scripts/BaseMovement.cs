5ff9effe2af4d6190cf0389e16c13a106d513611
kat weis <kxw4589@g.rit.edu>
2018-02-27 18:11:19 -0500

    Created a base movement script and started on flocking, also made a prefab for the lumberjack/flocker

---
+++ Team7_Project1/Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseMovement : MonoBehaviour {
+
+    //vectors for movement
+    private Vector3 position;
+    private Vector3 velocity;
+    private Vector3 acceleration;
+    private Vector3 direction;
+
+    private Vector3 desiredVelocity;
+
+    //vectors for movement
+    public float maxSpeed;
+    public float radius;
+
+    private TerrainData tData;//reference to the terrain data
+
+    // Use this for initialization
+    void Start () {
+        tData = GameObject.Find("Terrain").GetComponent<TerrainData>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CalcSteeringForces();
+        UpdatePosition();
+        SetTransform();
+    }
+
+    /// <summary>
+    /// Determines the acceleration
+    /// by applying the sum of the steering forces
+    /// </summary>
+    protected abstract void CalcSteeringForces();
+
+    /// <summary>
+	/// UpdatePosition
+	/// Calculate the velocity and resulting position of an entity
+	/// based on any forces
+	/// </summary>
+	protected void UpdatePosition()
+    {
+        position = transform.position;
+        //add acceleration to velocity
+        velocity += acceleration * Time.deltaTime;
+        //clamp velocity
+        Vector3.ClampMagnitude(velocity, maxSpeed);
+        velocity.y = 0;
+        //add velocity to position
+        position += velocity * Time.deltaTime;
+        position.y = tData.GetHeight((int)position.x, (int)position.z); //sets the Y position so that the entities sit on top of the terrain, may need adjustment for height of the model
+        //calculate direction from velocity
+        direction = velocity.normalized;
+        //zero out acceleration
+        acceleration = Vector3.zero;
+    }
+
+    /// <summary>
+    /// calculates the force to cause current object to seek a target
+    /// </summary>
+    /// <param name="targetPosition">position of object being saught</param>
+    /// <returns>Seek force</returns>
+	protected Vector3 Seek(Vector3 targetPosition)
+    {
+        //step 1: calculate desired velocity
+        //vector from myself to the target
+        desiredVelocity = targetPosition - position;
+        //step 2: scale to maxspeed
+        desiredVelocity = maxSpeed * desiredVelocity.normalized;
+        //step 3: calculate the steering force
+        desiredVelocity -= velocity;
+        //step 4: return steering force
+        return desiredVelocity;
+    }
+
+    /// <summary>
+    /// calculates the force to cause current object to flee a target
+    /// </summary>
+    /// <param name="targetPosition">position of object currently fleeing from</param>
+    /// <returns>Flee force</returns>
+    protected Vector3 Flee(Vector3 targetPosition)
+    {
+        //step 1: calculate desired velocity
+        //vector from myself to the target - negated so it flees
+        desiredVelocity = -(targetPosition - position);
+        //step 2: scale to maxspeed
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+        //step 3: calculate the steering force
+        desiredVelocity -= velocity;
+        //step 4: return steering force
+        return desiredVelocity;
+    }
+
+    protected Vector3 ObstacleAvoidance(Vector3 obstPos)
+    {
+        return Vector3.zero;
+    }
+
+    /// <summary>
+    /// Applies any Vector3 force to the acceleration vector
+    /// Assuming mass of 1 in this case
+    /// </summary>
+    /// <param name="force">Force.</param>
+    public void ApplyForce(Vector3 force)
+    {
+        acceleration += force;
+    }
+
+    /// <summary>
+    /// Sets the transform component to the local positon
+    /// </summary>
+    void SetTransform()
+    {
+        //set forward vector equal to the entity's direction
+        transform.forward = direction;
+
+        transform.position = position;
+    }
+}
--- Team7_Project1/Assets/Scripts/Flock.cs
+++ Team7_Project1/Assets/Scripts/Flock.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flock : MonoBehaviour
+public class Flock : BaseMovement
 {
     FlockingManager manager;
     public enum FlockState
@@ -32,11 +32,34 @@ public class Flock : MonoBehaviour
 
     float movementSpeed = 5;
 
+    //weights for the 3 separate parts of flocking
+    public float sepWgt;
+    public float alignWgt;
+    public float cohWgt;
+
+    //whether or not the parts of flocking are turned on
+    private bool separationOn;
+    private bool alingmentOn;
+    private bool cohesionOn;
+
+    public float maxForce; //how quickly they can turn/accelerate
+    private Vector3 ultForce;//the vector3 that determines acceleration
+
+    public float flockRadius;//how far away a flocker can be from this flocker to be considered part of its flock
+    private Vector3 flockCenter;//point in the middle of this flocker's flock
+    private Vector3 flockDirection;//direction of this flocker's flock
+
+
     Vector3 targetPostion;
 
     // Use this for initialization
     void Start () {
         manager = GameObject.Find("FlockingManager").GetComponent<FlockingManager>();
+
+        //set the flocking forces to be on
+        separationOn = true;
+        alingmentOn = true;
+        cohesionOn = true;
 	}
 	
 	// Update is called once per frame
@@ -46,18 +69,115 @@ public class Flock : MonoBehaviour
         {
             PassBottleneck();
         }
+
+        //toggle the different forces in flocking
+        if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            cohesionOn = !cohesionOn;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            alingmentOn = !alingmentOn;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            separationOn = !separationOn;
+        }
     }
 
-    public void Separation()
+    protected override void CalcSteeringForces()
     {
+        //reset ultForce
+        ultForce = Vector3.zero;
+        //apply all flocking forces as default, but allow toggling of parts by keyboard input
+        if (cohesionOn == true)
+        {
+            ultForce += cohWgt * Cohesion();
+        }
+        if (alingmentOn == true)
+        {
+            ultForce += alignWgt * Alignment();
+        }
+        if (separationOn == true)
+        {
+            ultForce += sepWgt * Separation();
+        }
+
+        //limit by max force
+        Vector3.ClampMagnitude(ultForce, maxForce);
+        //apply ultimate force
+        ApplyForce(ultForce);
     }
 
-    public void Cohesion()
+    /// <summary>
+    /// Katarina Weis
+    /// Will search through all flockers in the scene 
+    /// Determine which ones are within its radius of awareness
+    /// Consider those flockers its flock
+    /// Set the center and direction of the flock appropriately
+    /// </summary>
+    private void DetectFlock()
     {
+        GameObject[] flock = null;
+        GameObject[] all = GameObject.FindGameObjectsWithTag("flocker");//get all flockers
+
+        //reset center and direction
+        flockCenter = Vector3.zero;
+        flockDirection = Vector3.zero;
+
+        for (int i = 0; i < all.Length; i++)
+        {
+            //add flockers within the radius to this flockers flock
+            if (Vector3.Distance(all[i].transform.position, transform.position) < flockRadius)
+            {
+                flock[flock.Length] = all[i];
+            }
+        }
+
+        //determine the center of the flock and the average direction for use in flocking algorithm
+        for (int i = 0; i < flock.Length; i++)
+        {
+            flockCenter += flock[i].transform.position;
+            flockDirection += flock[i].transform.forward;
+        }
+
+        flockCenter = flockCenter / flock.Length;
+        flockDirection.Normalize();
     }
 
-    public void Alignment()
+    /// <summary>
+    /// Katarina Weis
+    /// Push the flocker slightly away from the other flockers in the flock
+    /// Flee the other flockers in the flock
+    /// Divide by the distance between them so the force is bigger the closer they are
+    /// <returns>Separation force</returns>
+    /// </summary>
+    public Vector3 Separation()
     {
+        return Vector3.zero;
+    }
+
+    /// <summary>
+    /// Katarina Weis
+    /// Push this flocker closer to the center of the flock
+    /// Causes bunching up
+    /// Seek the center of the flock
+    /// <returns>Cohesion force</returns>
+    /// </summary>
+    public Vector3 Cohesion()
+    {
+        return Seek(flockCenter);
+    }
+
+    /// <summary>
+    /// Katarina Weis
+    /// Direct the flocker in the direction of the rest of the flock
+    /// Lines up the direction of the flocker with the average of the flock
+    /// <returns>Alignment force</returns>
+    /// </summary>
+    public Vector3 Alignment()
+    {
+        return Vector3.zero;
     }
 
     public void PassBottleneck()
